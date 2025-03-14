283fce93b4dae7058d6ac7ec613b8e78f287a3f5
Eric Sun <ericsun987@gmail.com>
2025-03-13 19:04:28 -0700

    lab complete

---
+++ Lab3.py
@@ -0,0 +1,92 @@
+import argparse
+import csv
+import sys
+
+def splitData(data):
+    header = []
+    rows = []
+    with open(data) as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        rows = [row for row in reader]
+
+    training_data = rows[:int(len(rows) * 0.8)]
+    testing_data = rows[int(len(rows) * 0.8):]
+
+    with open("DATA/TrainingData.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(training_data)
+
+    with open("DATA/TestingData.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(testing_data)
+
+import random
+def splitDataRandom(data):
+    header = []
+    rows = []
+    with open(data) as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        rows = [row for row in reader]
+    
+    random.shuffle(rows)
+    training_data = rows[:int(len(rows) * 0.8)]
+    testing_data = rows[int(len(rows) * 0.8):]
+
+    with open("DATA/TrainingData.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(training_data)
+
+    with open("DATA/TestingData.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(testing_data)
+
+def showHelper():
+    parser.print_help(sys.stderr)
+    print("Please provide input augument. Here are examples:")
+    print("python " + sys.argv[0] + " --input DATA/CrabAgePrediction.csv")
+    print("python " + sys.argv[0] + " --mode r --input DATA/CrabAgePrediction.csv")
+    sys.exit(0)
+
+def main():
+    options = parser.parse_args()
+    mode = options.mode
+    print("mode is " + mode)
+    if mode == '':
+        """
+        The training mode
+        """
+        inputFile = options.input
+        if inputFile == '':
+            showHelper()
+        splitData(inputFile)
+    elif mode == "r":
+        inputFile = options.input
+        print(inputFile)
+        if inputFile == '':
+            showHelper()
+        splitDataRandom(inputFile)
+    else:
+        showHelper
+    pass
+
+if __name__ == "__main__":
+    #------------------------arguments------------------------------#
+    #Shows help to the users                                        #
+    #---------------------------------------------------------------#
+    parser = argparse.ArgumentParser()
+    parser._optionals.title = "Arguments"
+    parser.add_argument('--mode', dest='mode',
+    default = '',    # default empty!
+    help = 'Mode: empty for sequential split 80/20, r for random split 80/20.')
+    parser.add_argument('--input', dest='input',
+    default = '',    # default empty!
+    help = 'The input file to be split.')
+    if len(sys.argv)<3:
+        showHelper()
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # CS330-Lab3
+
+## Dataset description (What is the dataset about?)
+The dataset contains physical attributes of farmed Crabs.
+
+## Input features (What do the features represent?)
+The features represent the phyisical attributes of crabs.
+
+## Output labels (What does the dataset predict?)
+The dataset predicts the age of the crab based on its other physical attributes.
+
+## Dataset source (Where did you find it?)
+Kaggle
+
+## How to run the code?
+Make sure you're in the root folder of the repository.
+
+commands: 
+```
+python Lab3.py --input [file name] 
+python Lab3.py --input [file name] --mode r
+```
+
+Testing and training files will appear in the DATA directory.
+
+## Group member contributions
+Eric Sun 100%
